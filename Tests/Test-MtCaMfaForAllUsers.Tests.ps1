378787b3869230f9869d50da0006dfb64e7823a4
lilian-17 <lilian.martpro@gmail.com>
2025-06-10 14:04:33 +0200

    Create Test-MtCaMfaForAllUsers.Tests.ps1

---
+++ Tests/Test-MtCaMfaForAllUsers.Tests.ps1
@@ -0,0 +1,10 @@
+  Describe "Cus" -Tag "Custom", "Security" {
+    It "Checks if the tenant has at least one conditional access policy requiring multifactor authentication for all users" {
+      $result = Test-MtCaMfaForAllUsers
+
+      if ($null -ne $result) {
+        $result | Should -Be $true -Because "MFA for all users conditional access policy can be used to require MFA for all users in the tenant."
+
+      }
+   }
+}
