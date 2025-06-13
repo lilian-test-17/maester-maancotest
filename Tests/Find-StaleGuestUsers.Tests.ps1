2fa070a737f9e00afb1b07b4730658f8e160a061
lilian-17 <lilian.martpro@gmail.com>
2025-06-13 10:07:44 +0200

    Update Find-StaleGuestUsers.Tests.ps1

--- Tests/Find-StaleGuestUsers.Tests.ps1
+++ Tests/Find-StaleGuestUsers.Tests.ps1
@@ -1,11 +1,30 @@
 # Requires -Module Pester
 
-Describe "Find-StaleGuestUsers" {
+Describe "Find-StaleGuestUsers" -Tag "Custom", "Users" {
     BeforeAll {
         . "$PSScriptRoot\Find-StaleGuestUsers.ps1"
     }
 
-    It "should return guests who haven't accepted invitation and are older than expiration threshold" {
+    It "CUS.004: Should return guests who haven't accepted invitation and are older than expiration threshold" {
+
+    try {
+            $policies = Get-MgIdentityConditionalAccessPolicy -All
+
+            $disabledWithoutReason = $policies | Where-Object { $_.State -eq "Disabled" -and $_.DisplayName -notlike "*Disabled:*" }
+
+            $testDescription = "Checks if the disabled policies have the reason for being disabled."
+            if ($disabledWithoutReason.Count -gt 0) {
+                $result = "There are $($disabledWithoutReason.Count) disabled policies without a reason for being disabled."
+                Add-MtTestResultDetail -Description $testDescription -Result $result
+            } else {
+                Add-MtTestResultDetail -Description $testDescription -Result "Well done. All disabled policies have a reason for being disabled."
+            }
+        } catch {
+            Write-Error $_.Exception.Message
+        }
+
+        $disabledWithoutReason | Should -Be 0
+
         $fakeDate = (Get-Date).AddDays(-31)
 
         Mock -CommandName Get-MgUser -MockWith {
